287a54dcfb9c5e0133b01e4a30df458d6add9c53
do3_173 <edo.fejzic@hotmail.com>
2023-10-25 02:59:44 +0200

    Knight's Tour

---
+++ ANNT_PSI_2021/Knight's Tour /dfs_warnsdorffs_knights_tour.py
@@ -0,0 +1,163 @@
+from collections import defaultdict
+import os
+from typing import List, Tuple, Optional
+from plot_knights_tour import plot_knight_tour
+import argparse
+
+size_of_board = 8
+start_square = (0,0)
+
+MOVE_OFFSETS = (
+    (-1, -2),
+    (1, -2),
+    (-2, -1),
+    (2, -1),
+    (-2, 1),
+    (2, 1),
+    (-1, 2),
+    (1, 2),
+)
+
+def add_edge(graph, vertex_a, vertex_b):
+    """
+    Add an edge to the graph between vertex_a and vertex_b.
+    
+    Parameters:
+        - graph (dict): The graph to which the edge should be added.
+        - vertex_a (tuple): One vertex of the edge.
+        - vertex_b (tuple): The other vertex of the edge.
+    """
+    graph[vertex_a].add(vertex_b)
+    graph[vertex_b].add(vertex_a)
+
+def build_graph(board_size: int) -> defaultdict:
+    """
+    Construct a graph based on a chessboard of given size.
+    
+    Parameters:
+        - board_size (int): Size of the side of the square chessboard.
+        
+    Returns:
+        - defaultdict: Graph representing legal moves on the chessboard.
+    """
+    graph = defaultdict(set)
+    for row in range(board_size):
+        for col in range(board_size):
+            for to_row, to_col in legal_moves_from(row, col, board_size):
+                add_edge(graph, (row, col), (to_row, to_col))
+    return graph
+
+def legal_moves_from(row: int, col: int, board_size: int) -> Tuple[int, int]:
+    """
+    Generator for all legal moves from a given position on the board.
+    
+    Parameters:
+        - row (int): Starting row.
+        - col (int): Starting column.
+        - board_size (int): Size of the side of the square chessboard.
+        
+    Yields:
+        - tuple: A tuple representing the legal move from the starting position.
+    """
+    for row_offset, col_offset in MOVE_OFFSETS:
+        move_row, move_col = row + row_offset, col + col_offset
+        if 0 <= move_row < board_size and 0 <= move_col < board_size:
+            yield move_row, move_col
+
+def warnsdorffs_heuristic(graph: defaultdict) -> callable:
+    """
+    Warnsdorff's heuristic for the knight's tour problem. Computes the degree of a vertex.
+    
+    Parameters:
+        - graph (defaultdict): Graph representing the chessboard.
+        
+    Returns:
+        - callable: A function that computes the degree of a vertex in the graph.
+    """
+    def compute_degree(vertex):
+        return len(graph[vertex])
+    return compute_degree
+
+def first_true(sequence: List[bool]) -> Optional[bool]:
+    """
+    Returns the first True value in the sequence, or None if there isn't one.
+    
+    Parameters:
+        - sequence (List[bool]): Sequence of boolean values.
+        
+    Returns:
+        - bool/None: The first True value or None.
+    """
+    for item in sequence:
+        if item:
+            return item
+    return None
+
+def find_knights_tour(board_size: int = 8, 
+                      start_row: int = 0, 
+                      start_col: int = 0, 
+                      heuristic: callable = warnsdorffs_heuristic,
+                      visualize: bool = True, 
+                      output_filename_animation: str = 'animated_knight_tour.gif') -> List[Tuple[int, int]]:
+    """
+    Finds a solution for the knight's tour problem using a given heuristic and visualizes the solution if specified.
+    
+    Parameters:
+        - board_size (int, optional): Size of the chessboard. Defaults to 8.
+        - start_row (int, optional): Row of the starting position. Defaults to 0.
+        - start_col (int, optional): Column of the starting position. Defaults to 0.
+        - heuristic (callable, optional): Heuristic function to use. Defaults to warnsdorffs_heuristic.
+        - visualize (bool, optional): Whether to visualize the solution. Defaults to True.
+        - output_filename_animation (str, optional): Filename for the saved animation. Defaults to 'animated_knight_tour.gif'.
+        
+    Returns:
+        - List[Tuple[int, int]]: A list of tuples representing the solution path.
+    """
+    graph = build_graph(board_size)
+    total_squares = board_size * board_size
+
+    def traverse(path, current_vertex):
+        if len(path) + 1 == total_squares:
+            print("Found Knight's Tour")
+            return path + [current_vertex]
+
+        yet_to_visit = graph[current_vertex] - set(path)
+        if not yet_to_visit:
+            return False
+
+        next_vertices = sorted(yet_to_visit, key=heuristic(graph))
+        return first_true(
+            traverse(path + [current_vertex], vertex) for vertex in next_vertices
+        )
+
+    solution = traverse([], (start_row, start_col))
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    output_path = os.path.join(script_dir + '/solutions', f"{board_size}x{board_size}_{start_row}-{start_col}.txt")
+
+    with open(output_path, "w") as output:
+        output.write("\n".join("{},{}".format(x[0], x[1]) for x in solution))
+
+    if visualize:
+        plot_knight_tour(solution, output_filename=output_filename_animation)
+
+    return solution
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Find a solution for the knight's tour problem.")
+    
+    parser.add_argument("--board_size", type=int, default=8, help="Size of the chessboard.")
+    parser.add_argument("--start_row", type=int, default=0, help="Row of the starting position.")
+    parser.add_argument("--start_col", type=int, default=0, help="Column of the starting position.")
+    parser.add_argument("--visualize", action="store_true", help="Whether to visualize the solution.")
+    parser.add_argument("--output_filename_animation", type=str, default="animated_knight_tour.gif", help="Filename for the saved animation.")
+    
+    args = parser.parse_args()
+    
+    solution = find_knights_tour(
+        board_size=args.board_size,
+        start_row=args.start_row,
+        start_col=args.start_col,
+        visualize=args.visualize,
+        output_filename_animation=args.output_filename_animation
+    )
---
+++ ANNT_PSI_2021/Knight's Tour /plot_knights_tour.py
@@ -0,0 +1,97 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+def parse_solution_from_file(filename):
+    """
+    Reads a knight's tour solution from a file and converts it into a list of coordinate pairs.
+    
+    Parameters:
+        - filename (str): The name of the file containing the solution.
+        
+    Returns:
+        - list[tuple[int, int]]: A list of coordinate pairs representing the knight's moves.
+    """
+    with open(filename, 'r') as f:
+        return [tuple(map(int, line.split(','))) for line in f.readlines()]
+
+def plot_knight_tour(solution, output_filename='animated_knight_tour.gif'):
+    """
+    Plots and saves an animated GIF of the knight's tour based on the provided solution.
+    
+    Parameters:
+        - solution (list[tuple[int, int]]): A list of coordinate pairs representing the knight's moves.
+        - output_filename (str, optional): The filename for the saved GIF. Defaults to 'animated_knight_tour.gif'.
+    """
+    
+    # Determine the size of the chessboard from the solution
+    board_dimension = int(np.sqrt(len(solution)))
+    
+    # Initialize the canvas with a margin around the chessboard
+    canvas_size = int(1.5 * board_dimension)
+    canvas = np.ones((canvas_size, canvas_size, 3))
+
+    # Calculate where the chessboard starts and ends on the canvas
+    chessboard_start = (canvas_size - board_dimension) // 2
+    chessboard_end = chessboard_start + board_dimension
+
+    # Fill the canvas with alternating black and white squares
+    for i in range(chessboard_start, chessboard_end):
+        for j in range(chessboard_start, chessboard_end):
+            if (i + j) % 2:
+                canvas[i, j] = [0, 0, 0]  # Black square
+
+    frames = []
+
+    # Create frames for each move in the solution
+    for index, coord in enumerate(solution):
+        fig, ax = plt.subplots(figsize=(8, 8))
+
+        # Color the path the knight has moved so far
+        for past_coord in solution[:index]:
+            x, y = past_coord
+            x += chessboard_start
+            y += chessboard_start
+            if (x + y) % 2:  # Black square
+                canvas[x, y] = [0, 0.5, 0]  # Dark Green
+            else:  # White square
+                canvas[x, y] = [0, 1, 0]  # Green
+
+        ax.imshow(canvas, origin='upper')
+        
+        # Mark the current position of the knight with a 'K'
+        k_y, k_x = coord
+        k_x += chessboard_start
+        k_y += chessboard_start
+        ax.text(k_x, k_y, 'K', color='black' if (k_x + k_y) % 2 == 0 else 'white', 
+                ha='center', va='center', fontsize=12, fontweight='bold')
+
+        # Draw a rectangle around the chessboard
+        rect = plt.Rectangle((chessboard_start - 0.5, chessboard_start - 0.5), 
+                             board_dimension, board_dimension, fill=False, color='black', linewidth=2)
+        ax.add_patch(rect)
+
+        # Add labels for the rows and columns of the chessboard
+        letters = [chr(97 + i) for i in range(board_dimension)]
+        numbers = list(range(1, board_dimension + 1))
+        for i, letter in enumerate(letters):
+            ax.text(chessboard_start + i, chessboard_end + 0.2, letter, ha='center', va='bottom', fontsize=12)
+            ax.text(chessboard_start - 0.8, chessboard_end - 1 - i, str(numbers[i]), ha='right', va='center', fontsize=12)
+        
+        # Add title to the frame
+        starting_square = f"{letters[solution[0][0]]}{numbers[-solution[0][1]-1]}"
+        ax.text(chessboard_start + board_dimension/2 - 0.25, chessboard_start - 1.5, f"Knight's Tour ({starting_square})", ha='center', va='top', fontsize=14, fontweight='bold')
+
+        ax.axis('off')
+        fig.tight_layout(pad=0)
+
+        # Convert the current frame to an array and add it to the frames list
+        fig.canvas.draw()
+        frame = np.frombuffer(fig.canvas.buffer_rgba(), dtype=np.uint8).reshape(fig.canvas.get_width_height()[::-1] + (4,))
+        frames.append(frame[..., :3])
+
+        plt.close(fig)
+
+    # Save the frames as an animated GIF
+    frames = [Image.fromarray(frame) for frame in frames]
+    frames[0].save('animations/' + output_filename, save_all=True, append_images=frames[1:], duration=500, loop=0)
